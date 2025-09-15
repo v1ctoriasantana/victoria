f942c6eaa085a713f23a2569829e54513c929466
Victoria <victoriasantana@berkeley.edu>
2025-09-15 14:12:45 -0700

    moved test.py

---
+++ homework1/homework1.py
@@ -0,0 +1,43 @@
+# File: homework1.py
+
+# --- Variables and Data Types ---
+a = 10
+print(a)
+print(type(a)) # a is an integer, a whole number with no decimals
+b = 1.5
+print (b)
+print(type(b)) # b is a float, a number with decimals
+c = 3j
+print (c)
+print(type(c)) # c is complex, which contains both a real and imaginary part 
+d = "hello"
+print(d)
+print(type(d)) # d is a string, a sequence of characters 
+e = [1, 2, 3]
+print(e)
+print(type(e)) # e is a list, an ordered collection of items
+f = {"name":  "Ellen", "favorite fruit":  "strawberry"}
+print(f)
+print(type(f)) # f is a dictionary, a data type used to store data values in key value pairs
+g = (1, 2)
+print(g)
+print(type(g)) # g is a tuple, an ordered, immutable collection of items
+h = ["apple", "banana", "strawberry"]
+print(h)
+print(type(h)) # h is a list, an ordered collection of items
+i = True
+print(i)
+print(type(i)) # i is a bool, a dara type that has one of two possible values, true or false
+j = None
+print(j)
+print(type(j)) # j a is a NoneType, a data type that represents the absence of a value 
+k = [True, "blue", 12]
+print(k)
+print(type(k)) # k is a list, an ordered collection of items
+l = str(14)
+print(l)
+print(type(l)) # l is a string, a sequence of characters 
+m = 1e4
+print(m)
+print(type(m)) # m is a float, a real number that can have fractional components
+# I found 
